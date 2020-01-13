4b17cfffde003f1e794858c4ceb70d5125abfd2e
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-01-13 20:08:56 +0100

    Imported Spark libraries into the project

--- src/CountAccidentsYearCompany.scala
+++ src/CountAccidentsYearCompany.scala
@@ -1,33 +1,47 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.{monotonically_increasing_id, explode, first}
 import scala.collection.mutable.HashMap 
+import scala.io.Source
 
 object CountAccidentsYearCompany extends App {
-     val hashMap:HashMap[String,HashMap[String,Integer]] = 
-      HashMap(("2019",HashMap()),
-              ("2018",HashMap()),
-              ("2017",HashMap()),
-              ("2016",HashMap()),
-              ("2015",HashMap()),
-              ("2014",HashMap()))
-    
-    val yearList = Array("2019","2018","2017","2016","2015","2014")
-    
-    val bufferedSource = io.Source.fromFile("C://Users//Christian//Desktop//JoachimsPaper//SelfDrivingCars.csv")
-    for (line <- bufferedSource.getLines()) {
-        val cols = line.split(",").map(_.trim)
-        for (i <- 0 to cols.length){
-          val words = cols(i).split(" ").map(_.trim) 
-          val name = words(0)
-          println(words(0))
-          //val companyMap:HashMap[String,Integer] = hashMap(yearList(i))
-          //if(companyMap.contains(words(0)))
-           // count = companyMap(words(0)
-          
-        }
-        // do whatever you want with the columns here
-        //println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}|${cols(4)}")
-    }
-    bufferedSource.close
-    
-    
-    //Count occurrences by company and year
+	var hashMap:HashMap[String,HashMap[String,Integer]] = 
+			HashMap(
+			    ("2019",HashMap()),
+					("2018",HashMap()),
+					("2017",HashMap()),
+					("2016",HashMap()),
+					("2015",HashMap()),
+					("2014",HashMap())
+					)
+
+			val yearList = Array("2019","2018","2017","2016","2015","2014")
+      var year = "2019"
+			val bufferedSource = scala.io.Source.fromFile("C://Users//Christian//Desktop//JoachimsPaper//SelfDrivingCars.csv")
+			for (line <- bufferedSource.getLines()) {
+				val cols = line.split(",")
+						for (i <- 0 to cols.length-1){
+						  year = yearList(i)
+							val words = cols(i).split(" ").map(_.trim) 
+							if(words!=null){
+										val name = words(0).replace('"',' ').trim()
+										var companyMap = hashMap(year)
+										var count = 1
+										if(companyMap.contains(name)){
+										  count = companyMap(name)
+										  count += 1
+										  println(count,name,year)
+										}
+										companyMap.put(name,count)
+										hashMap.put(year,companyMap)
+							}
+							else println("words null" + cols)
+						}
+			}
+			
+			println(hashMap(year))
+			
+bufferedSource.close
+
+//Count occurrences by company and year
 }
