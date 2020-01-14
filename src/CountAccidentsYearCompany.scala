0bf2bbd795895d5a1b1841a0835271df8955666c
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-01-14 21:42:36 +0100

    Finish exporting to CVS format.

--- src/CountAccidentsYearCompany.scala
+++ src/CountAccidentsYearCompany.scala
@@ -9,8 +9,13 @@ import java.nio.charset.StandardCharsets
 import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization
 
+import java.io._
+
+
+
+
 object CountAccidentsYearCompany extends App {
-	var hashMap:HashMap[String,HashMap[String,Integer]] = 
+	var hashMap:HashMap[String,HashMap[String,Int]] = 
 			HashMap(
 			    ("2019",HashMap()),
 					("2018",HashMap()),
@@ -23,7 +28,9 @@ object CountAccidentsYearCompany extends App {
 			val yearList = Array("2019","2018","2017","2016","2015","2014")
       var year = "2019"
 			val bufferedSource = scala.io.Source.fromFile("C://Users//Christian//Desktop//JoachimsPaper//SelfDrivingCars.csv")
-			for (line <- bufferedSource.getLines()) {
+			var lineIter = bufferedSource.getLines().drop(0) //discard header
+			lineIter.next()
+			for (line <- lineIter) {
 				val cols = line.split(",")
 						for (i <- 0 to cols.length-1){
 						  year = yearList(i)
@@ -31,7 +38,7 @@ object CountAccidentsYearCompany extends App {
 							if(words!=null){
 										val name = words(0).replace('"',' ').trim()
 										var companyMap = hashMap(year)
-										var count = 1
+										var count:Int = 1
 										if(companyMap.contains(name)){
 										  count = companyMap(name)
 										  count += 1
@@ -54,9 +61,26 @@ object CountAccidentsYearCompany extends App {
 			//val list: List[String] = List("a", "aa")
 			//val map = Map("query" -> yourBytes, "abc" -> list)
 			val json = Serialization.writePretty(hashMap.toMap)
-			println(json)
+			//println(json)
 
+			var listBuffer = new scala.collection.mutable.ListBuffer[String]()
+			for ((k_year,v_map) <- hashMap){
+			  for((k_company,v_count) <- v_map){
+			    listBuffer += new String(k_year+","+k_company+","+v_count.toString()+"\n")
+			  }
+			}
+			// println(listBuffer(1))
 
+			  
+			val file = new File("C://Users//Christian//Desktop//JoachimsPaper//AccidentCount.csv")
+			var bw = new BufferedWriter(new FileWriter(file))
+			bw.write(new String("year,company,accidents"+"\n"))
+			listBuffer.foreach {
+			   bw.write
+			}
+			bw.close()
+			
+			
 //Write results into a Spark dataframe
 //val DataFrame df = df.withColumn("id", (org.apache.spark.sql.functions.monotonically_increasing_id()))
 // .select($"id", explode(hashMap))
